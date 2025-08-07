e713d1b5225eb3be0e111a4ee6a8fcba4f14065b
kmartins <kelvin.martins@sempreceub.com>
2025-08-07 11:40:49 -0300

    Finalização do exercicio proposto

--- parte_3/src/ArquivosEmJava/EPItensVendidoscsv/Program.java
+++ parte_3/src/ArquivosEmJava/EPItensVendidoscsv/Program.java
@@ -2,13 +2,15 @@ package ArquivosEmJava.EPItensVendidoscsv;
 
 import ArquivosEmJava.EPItensVendidoscsv.entities.Product;
 
+import java.io.*;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
@@ -16,12 +18,43 @@ public class Program {
         List<Product> list = new ArrayList<>();
 
         System.out.print("Entre com o arquivo para leitura: ");
-        String rootPath = sc.nextLine();
+        String sourceFileStr = sc.nextLine();
 
-        try(){
+        File sourceFile = new File(sourceFileStr);
+        String sourceFolderStr = sourceFile.getParent();
 
-        }catch (){
+        boolean success = new File(sourceFolderStr + "\\out").mkdir();
 
+        String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+            String linesCsv = br.readLine();
+            while (linesCsv != null) {
+                String[] fields = linesCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Product(name, price, quantity));
+
+                linesCsv = br.readLine();
+            }
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+                for (Product item : list) {
+                    bw.write(item.getProduct() + ", " + String.format("%.2f", item.total()));
+                    bw.newLine();
+                }
+
+                System.out.println(targetFileStr + " Criado!");
+
+            } catch (IOException e) {
+                System.out.println("Erro na escrita: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro:" + e.getMessage());
         }
 
 
